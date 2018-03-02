091350aadad98e5904e4ef571b4f92422d5b3a0a
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2018-03-03 01:21:37 +0700

    пустой проект создал.

---
+++ app/src/main/java/ru/goodibunakov/cartoonquiz/SplashActivity.java
@@ -0,0 +1,13 @@
+package ru.goodibunakov.cartoonquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
