38bfc3e7c8cd74f67a08928a14726a9ba9f94086
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2018-03-18 23:12:36 +0700

    сделал лого на основном экране

--- app/src/main/java/ru/goodibunakov/cartoonquiz/MainActivity.java
+++ app/src/main/java/ru/goodibunakov/cartoonquiz/MainActivity.java
@@ -1,10 +1,12 @@
 package ru.goodibunakov.cartoonquiz;
 
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView rays = findViewById(R.id.main_rays);
+        Animation animationRotateCenter = AnimationUtils.loadAnimation(
+                this, R.anim.rotate_center);
+        rays.startAnimation(animationRotateCenter);
     }
 
     @Override
@@ -35,7 +42,13 @@ public class MainActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                             | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN); // hide status bar
         }
-        Log.i("HideSystemUI work", "cool");
     }
 }
--- app/src/main/java/ru/goodibunakov/cartoonquiz/SplashActivity.java
+++ app/src/main/java/ru/goodibunakov/cartoonquiz/SplashActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -72,8 +71,14 @@ public class SplashActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                             | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN); // hide status bar
         }
-        Log.i("HideSystemUI work", "cool");
     }
 
 
