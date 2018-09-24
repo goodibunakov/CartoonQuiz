c93400df04be8d76e3243e3e9ddbcb1a9e9bc04c
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2018-09-24 21:39:44 +0700

    Migrate to AndroidX

--- app/src/main/java/ru/goodibunakov/cartoonquiz/MainActivityFragment.java
+++ app/src/main/java/ru/goodibunakov/cartoonquiz/MainActivityFragment.java
@@ -2,11 +2,11 @@ package ru.goodibunakov.cartoonquiz;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
--- app/src/main/java/ru/goodibunakov/cartoonquiz/SplashActivity.java
+++ app/src/main/java/ru/goodibunakov/cartoonquiz/SplashActivity.java
@@ -3,12 +3,12 @@ package ru.goodibunakov.cartoonquiz;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
