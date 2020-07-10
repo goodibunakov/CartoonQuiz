c9e321ce4c2f57e9bb4855d5c59d8f1a115a9ccf
Evgeny Bunakov <goodi@bk.ru>
2020-07-11 03:13:17 +0700

    update dependencies

--- app/src/main/java/ru/goodibunakov/cartoonquiz/QuizApp.java
+++ app/src/main/java/ru/goodibunakov/cartoonquiz/QuizApp.java
@@ -2,18 +2,10 @@ package ru.goodibunakov.cartoonquiz;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 public class QuizApp extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
+
     }
 }
